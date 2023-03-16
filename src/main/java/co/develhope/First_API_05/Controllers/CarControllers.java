1c61acc31cb248df0ccda62780ec0615afb68128
M19732004 <mariacoman73@gmail.com>
2023-03-16 23:42:02 +0100

    corectat body

--- src/main/java/co/develhope/First_API_05/Controllers/CarControllers.java
+++ src/main/java/co/develhope/First_API_05/Controllers/CarControllers.java
@@ -2,27 +2,24 @@ package co.develhope.First_API_05.Controllers;
 
 import co.develhope.First_API_05.DTO.CarDTO;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+        import jakarta.servlet.http.HttpServletResponse;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+        import javax.validation.Valid;
 
-@RestController
-@RequestMapping
-public class CarControllers {
-    @GetMapping("/")
-    public String get(String id,String modelCar, Double price){
-        return "Ciao ,sono il get "+id+" modelCar "+modelCar+" ,price ="+price;
-    }
-    @PostMapping("/")
-    public String postCar(@RequestBody(required = false) CarDTO carDTO,
-                          HttpServletRequest request, HttpServletResponse response){
-        return carDTO.toString();
-    }
-    @PostMapping("/users")
-    ResponseEntity<String> addUser(@Valid @RequestBody CarDTO carDTO) {
-        // persisting the user
-        return ResponseEntity.ok("User is valid");
-    }
-}
+        @RestController
+        @RequestMapping
+        public class CarControllers {
+            @GetMapping("/")
+            public String insertNewCar(@RequestBody CarDTO carDTO){
+
+                return carDTO.toString();
+            }
+            @PostMapping("/")
+            public String postCar(@RequestBody(required = false) CarDTO carDTO,
+                                  HttpServletRequest request, HttpServletResponse response){
+                return carDTO.toString();
+            }
+
+        }
--- src/main/java/co/develhope/First_API_05/DTO/CarDTO.java
+++ src/main/java/co/develhope/First_API_05/DTO/CarDTO.java
@@ -1,16 +1,32 @@
 package co.develhope.First_API_05.DTO;
 import lombok.Data;
-import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 import javax.validation.constraints.NotBlank;
 @Data
-@ToString
+
 public class CarDTO {
     @NotBlank(message = "Mandatory")
-    private String id;
+    private long id;
     @NotBlank(message = "Mandatory")
     private String modelName;
     @NotBlank(message = "Mandatory")
-    private double price;
+    private Double price;
 
+
+    public CarDTO(){
+
+    }
+
+    public CarDTO(long id, String modelName, double price) {
+        this.id = id;
+        this.modelName = modelName;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %d",id,modelName,price);
+    }
 }
