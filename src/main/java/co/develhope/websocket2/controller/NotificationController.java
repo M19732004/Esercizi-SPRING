89ea9504aeb525fdf57a60585236e1b548be4b7d
M19732004 <mariacoman73@gmail.com>
2023-04-13 20:10:02 +0200

    correzione esercizio

--- src/main/java/co/develhope/websocket2/controller/NotificationController.java
+++ src/main/java/co/develhope/websocket2/controller/NotificationController.java
@@ -1,9 +1,12 @@
 package co.develhope.websocket2.controller;
 
+import co.develhope.websocket2.entities.ClientMessageDTO;
 import co.develhope.websocket2.entities.MessageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +17,14 @@ public class NotificationController {
     @Autowired
     public SimpMessagingTemplate simpMessagingTemplate;
     @PostMapping("/notification")
-    public ResponseEntity sendNotificationToClients (@RequestBody MessageDTO message){
-        simpMessagingTemplate.convertAndSend("/topic/greetings",message);
-        return ResponseEntity.status(HttpStatus.OK).build();
+    public ResponseEntity sendNotificationToClients (@RequestBody MessageDTO messageDTO){
+        simpMessagingTemplate.convertAndSend("/topic/greetings",messageDTO);
+        return ResponseEntity.accepted().body("Message"+messageDTO.getMessage()+"sent to "+"/topic/broadcast");
+    }
 
+    @MessageMapping("/client-message")
+    @SendTo("/topic/broadcast")
+    public MessageDTO messageFromWebSocket (ClientMessageDTO message){
+        return new MessageDTO(message.getClientName(),message.getClientMsg(), message.getClientAlert());
     }
 }
